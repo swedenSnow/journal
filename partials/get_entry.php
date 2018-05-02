9648b3d2224b662d137a6e513085b7de1e5beb88
Hani Abu Zeid <31956031+swedenSnow@users.noreply.github.com>
2018-05-02 09:44:59 +0200

    delete update added

--- partials/get_entry.php
+++ partials/get_entry.php
@@ -1,17 +1,17 @@
 
 <div class="entry">
-  <form class="form" action="update-entry.php" method="POST">
+  <form class="form" action="delete_edit.php" method="POST">
     <h3 class="entry-title">
       <?= $post["title"]; ?>
     </h3>
     <p class="para">
       <?= $post["content"]; ?>
-</p>
+    </p>
     <p class="entry-date">
         <?=$post['createdAt']; ?>
     </p>
     <input type="text" name="entryID" id="hide-id" value="<?= $post["entryID"] ?>">
-    <input type="submit" class="" name="edit" value="edit">
-    <input type="submit" class="" name="remove" value="remove">
+    <input type="submit" class="edit-post" name="edit" value="edit">
+    <input type="submit" class="remove-post" name="remove" value="remove">
   </form>
 </div>
